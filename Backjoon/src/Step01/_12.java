32ab11b331cc0059aad5045912e7460c7acae104
garuina <a01074882716@gmail.com>
2022-08-20 23:42:04 +0900

    Backjoon 업뎃 0820

--- Backjoon/src/Step01/_12.java
+++ Backjoon/src/Step01/_12.java
@@ -11,15 +11,13 @@ public class _12 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();int b = sc.nextInt();int c = sc.nextInt();
-		int d = sc.nextInt();int e = sc.nextInt();int f = sc.nextInt();
-	
-		System.out.println( (a*100 + b*10 + c) * f );
-		System.out.println( (a*100 + b*10 + c) * e );
-		System.out.println( (a*100 + b*10 + c) * d );
-		System.out.println( (a*100 + b*10 + c) * (d*100 + e*10 + f) );
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
-	
+		System.out.println( a *(b - (b/10)*10 )); 
+		System.out.println( a * ( b/10 - b/100*10) );
+		System.out.println( a *( b /100));
+		System.out.println( a *b );
+		
 	}
-
 }
--- Backjoon/src/Step01/_14.java
+++ Backjoon/src/Step01/_14.java
@@ -1,5 +1,18 @@
 package Step01;
 
+/*
+ * 날짜 : 2022/08/20
+ * 이름 : 장인화
+ * 내용 : 1단계 13번 : 개
+ */
 public class _14 {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
 
+	}
 }
