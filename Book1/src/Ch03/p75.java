8700f575285c539be69454e9382d6581ed8a86d4
garuina <a01074882716@gmail.com>
2022-08-21 23:17:43 +0900

    Book1 22/08/21

---
+++ Book1/src/Ch03/p75.java
@@ -0,0 +1,13 @@
+package Ch03;
+
+public class p75 {
+	 public static void main(String[] args) {
+		int gamescore = 150;
+		
+		int lastscore1 = ++gamescore;
+		System.out.println(lastscore1);
+		
+		int lastscore2=--gamescore;
+		System.out.println(lastscore2);
+	}
+}
---
+++ Book1/src/Ch03/p81.java
@@ -0,0 +1,13 @@
+package Ch03;
+
+public class p81 {
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int MotherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > MotherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+	}
+}
