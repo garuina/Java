22e2734ef722ced18e77ab5132fc7e08f4458aac
a01074882716 <a01074882716@gmail.com>
2022-08-23 11:00:39 +0900

    백준 업뎃 08/23

--- Backjoon/src/Step02/_03.java
+++ Backjoon/src/Step02/_03.java
@@ -1,5 +1,20 @@
 package Step02;
 
-public class _03 {
+import java.util.Scanner;
 
+public class _03 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		
+		if( x % 4 == 0 && x % 100 !=0 || x % 400 ==0 ) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+		
+		
+	}
 }
