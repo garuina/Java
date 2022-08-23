b6d23f7167df763c7df76f602a99119568a77682
a01074882716 <a01074882716@gmail.com>
2022-08-23 13:53:25 +0900

    백준 5번까지

--- Backjoon/src/Step02/_05.java
+++ Backjoon/src/Step02/_05.java
@@ -1,5 +1,34 @@
 package Step02;
 
-public class _05 {
+import java.util.Scanner;
 
+public class _05 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if( H>0 && M>=45 ) {
+			System.out.print(H + " ");
+			System.out.print(M-45);
+		
+		}else if(H>0 && M<45) {
+			System.out.print(H-1 + " ");
+			System.out.print(60-(45-M) );
+		
+		}else if(H==0 && M>=45) {
+			System.out.print(0 + " ");
+			System.out.print(45-M);
+			
+				
+		}else {
+			System.out.print(23 + " ");
+			System.out.print(60-(45-M));
+		}
+		
+		
+		
+	}
 }
