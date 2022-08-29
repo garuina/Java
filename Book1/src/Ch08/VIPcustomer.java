ee253b95d04b08e6c8c21d1c7a331332762c9f8c
a01074882716 <a01074882716@gmail.com>
2022-08-29 13:51:07 +0900

    Book1 22/08/29

---
+++ Book1/src/Ch08/VIPcustomer.java
@@ -0,0 +1,25 @@
+package Ch08;
+
+public class VIPcustomer extends Customer {
+	
+	//속성
+	private int agentID;
+	double saleRatio;
+	
+	public VIPcustomer() {
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+	}
+	
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * saleRatio);
+	}
+	
+	public int getAgentID() {
+		return agentID;
+	}
+	
+	
+}
