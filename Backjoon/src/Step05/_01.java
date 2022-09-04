6aa2d6084efb74cbf2ba02bb2eed1bbca274e514
garuina <a01074882716@gmail.com>
2022-09-04 22:09:00 +0900

    22/09/04 집

---
+++ Backjoon/src/Step05/_01.java
@@ -0,0 +1,18 @@
+package Step05;
+
+
+public class _01 {
+	
+		
+		long sum(int[]a) {
+		long sum = 0;	
+		
+		for(int i=0; i<a.length; i++) {
+			sum += a [i];
+		}
+		return sum;
+		}
+}
+		
+	
+
---
+++ Backjoon/src/Step05/_03.java
@@ -0,0 +1,17 @@
+package Step05;
+
+import java.util.Scanner;
+
+public class _03 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		
+		
+		
+		
+		
+	}
+}
