d5313d08d3193d885f399c200a29db3ceaea695c
a01074882716 <a01074882716@gmail.com>
2022-09-05 13:44:48 +0900

    22/09/05 학원

---
+++ Book1/src/Ch14/P494.java
@@ -0,0 +1,15 @@
+package Ch14;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class P494 {
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("a.txt");
+		}catch(FileNotFoundException e) {
+			System.out.println(e);
+		}
+		System.out.println("여기도 수행됩니다.");
+	}
+}
