3397247b6ccc3fb87f54eba32453ebac951c84cb
garuina <a01074882716@gmail.com>
2022-09-08 00:14:11 +0900

    22/09/08 집

---
+++ Book1/src/Ch11/P358.java
@@ -0,0 +1,17 @@
+package Ch11;
+class Student{
+	int studentId;
+	String studentName;
+	
+	public Student(int studentId, String studentName) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
+	
+	public class EqualsTest{
+		return studentId + "," +studentName;
+	}
+}
+public class P358 {
+
+}
