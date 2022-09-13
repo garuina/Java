2eb294f3dc1dfda6bf9699251fc2e264f97df6a1
a01074882716 <a01074882716@gmail.com>
2022-09-13 12:44:47 +0900

    2022/09/13

--- Backjoon/src/Step05/_02.java
+++ Backjoon/src/Step05/_02.java
@@ -1,5 +1,16 @@
 package Step05;
 
 public class _02 {
+	
+	public void d(n){
+		int n = n + (n/10) +(n*10);
+	while(true) {
+		
+		if(n<10000) {
+			System.out.println(n);
+		}break;
+	}
+	}
 
+	
 }
