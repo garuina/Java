1040ff8902e288fe970855952b354d9333bea139
a01074882716 <a01074882716@gmail.com>
2022-09-14 10:44:00 +0900

    22/09/14

---
+++ Ch11/src/sub2/UpdateTest.java
@@ -0,0 +1,40 @@
+package sub2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class UpdateTest {
+	public static void main(String[] args) {
+		
+		try {
+		//DB정보
+		String host = "jdbc:mysql://127.0.0.1:3306/java1db";
+		String user = "root";
+		String pass = "1234";
+			
+		//1단계 - JDBC 드라이버 로드
+		Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		//2단계 - 데이터베이스 접속
+		Connection conn = DriverManager.getConnection(host, user, pass);
+		
+		//3단계 - SQL 실행개체 생성
+		Statement stmt = conn.createStatement();
+			
+		//4단계 - SQL 실행
+		String sql = "UPDATE `User1` SET `name`='홍길동', `hp`='010-1111-1111' ";
+			   sql += "WHERE `uid`= 'a101';";
+			
+		stmt.executeUpdate(sql);
+			   
+		//5단계 - 결과처리(SELECT 경우)
+			   
+		//6단계 - 데이처베이스 종료
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Update 완료...");
+		
+	}
+}
