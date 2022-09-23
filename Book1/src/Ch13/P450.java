90a6f9c20c248c75c0d75d8f08dc2191611140f1
a01074882716 <a01074882716@gmail.com>
2022-09-23 13:44:35 +0900

    2022/09/23

--- Book1/src/Ch13/P450.java
+++ Book1/src/Ch13/P450.java
@@ -18,18 +18,17 @@ class OutClass{
 		System.out.println("OutClss num = " + num + "(외부 클래스의 인스턴스 변수)");
 		System.out.println("OutClss snum = " + sNum + "(외부 클래스의 인스턴스 변수)");
 	}
+	}
 	public void usingClass() {
 		inClass.inTest();
 	}
 	}
-	
-	
-	
-	
-}
-
-
 
 public class P450 {
+	public static void main(String[] args) {
+		OutClass outclass = new OutClass();
+		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+		outclass.usingClass();
+	}
 
 }
