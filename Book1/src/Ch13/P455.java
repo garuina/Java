76f9abe91b3b22db07c6a9650620fe343fbdf650
a01074882716 <a01074882716@gmail.com>
2022-09-26 13:10:18 +0900

    2022/09/26

---
+++ Book1/src/Ch13/P455.java
@@ -0,0 +1,24 @@
+package Ch13;
+
+class Outer {
+	int outNum = 100;
+	static int sNum = 200;
+	
+	Runnable getRunnable(int i) {
+		int num = 100; //지역변수
+	 class MyRunnable implements Runnable {
+		 int localNum = 10;
+		 
+	@Override
+	public void run() {
+		
+		
+	}	 
+  }
+	 return new 
+}
+}
+
+public class P455 {
+
+}
