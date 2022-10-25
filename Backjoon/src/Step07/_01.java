32d7d4fb92daa201e6a76480c978c849365a6f2c
a01074882716 <a01074882716@gmail.com>
2022-10-25 13:51:02 +0900

    2022/10/25

--- Backjoon/src/Step07/_01.java
+++ Backjoon/src/Step07/_01.java
@@ -11,14 +11,14 @@ public class _01 {
 		int B = sc.nextInt();
 		int C = sc.nextInt();
 		
-<<<<<<< HEAD
+
 		if(C <= B) {
 			System.out.println("-1");
 		}
 		else {
 			System.out.println( (A/(C-B))+1 );
 		}
-=======
+
 		int cnt = 0;
 		
 		while(true) {
@@ -29,6 +29,6 @@ public class _01 {
 		}
 		System.out.println(cnt);
 		}
->>>>>>> 9fe5d58332ca8b1ffe8a098f201c71ea59f3e700
+
 	}
 
